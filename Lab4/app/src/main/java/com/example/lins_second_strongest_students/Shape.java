61318525cc3601f788abfd379fbee00583cb45cc
Khushdeep1337 <150091893+Khushdeep1337@users.noreply.github.com>
2024-01-31 19:32:52 -0700

    Colour change personal

--- Lab4/app/src/main/java/com/example/lins_second_strongest_students/Shape.java
+++ Lab4/app/src/main/java/com/example/lins_second_strongest_students/Shape.java
@@ -1,8 +1,9 @@
 package com.example.lins_second_strongest_students;
 
-public abstract class Shape {
+public abstract class Shape () {
     int x;
     int y;
+    String color = "Purple";
     
 }
 
