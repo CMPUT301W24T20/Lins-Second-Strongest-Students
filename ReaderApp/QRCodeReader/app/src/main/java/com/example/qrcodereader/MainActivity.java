b6aaa947054811e5dd6885443a58ec7c88533932
TheVinayJ <thevinayj@gmail.com>
2024-03-04 15:08:02 -0700

    Fixed UI navigation + Some QOL
    Home fragment now contains code originally in MainActivity
    Camera is now portrait mode
    Replaced 'Dashboard' with 'Scan'

--- ReaderApp/QRCodeReader/app/src/main/java/com/example/qrcodereader/MainActivity.java
+++ ReaderApp/QRCodeReader/app/src/main/java/com/example/qrcodereader/MainActivity.java
@@ -50,56 +50,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
-
-        // I'm working on this part - Duy
-        Button profile_button = findViewById(R.id.profile_button);
-        profile_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, OrganizerEventActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button MyEventButton = findViewById(R.id.my_event_button);
-        MyEventButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-
-                builder.setTitle("Choose an action");
-
-                // Button to go to AttendeeEventActivity
-                builder.setPositiveButton("Go to Your Event Page (Attendee)", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Intent intent = new Intent(MainActivity.this, AttendeeEventActivity.class);
-                        startActivity(intent);
-                    }
-                });
-
-                // Button to go to OrganizerEventActivity
-                builder.setNegativeButton("Go to Your Event Page (Organizer)", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Intent intent = new Intent(MainActivity.this, OrganizerEventActivity.class);
-                        startActivity(intent);
-                    }
-                });
-
-                // Cancel button
-                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog
-                        dialog.dismiss();
-                    }
-                });
-
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }
-        });
-
-
       
         String deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
         User user = new User(deviceID, "Guohui Lin");
--- ReaderApp/QRCodeReader/app/src/main/java/com/example/qrcodereader/ui/home/HomeFragment.java
+++ ReaderApp/QRCodeReader/app/src/main/java/com/example/qrcodereader/ui/home/HomeFragment.java
@@ -1,16 +1,24 @@
 package com.example.qrcodereader.ui.home;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.qrcodereader.MainActivity;
+import com.example.qrcodereader.R;
 import com.example.qrcodereader.databinding.FragmentHomeBinding;
+import com.example.qrcodereader.ui.eventPage.AttendeeEventActivity;
+import com.example.qrcodereader.ui.eventPage.OrganizerEventActivity;
 
 public class HomeFragment extends Fragment {
 
@@ -26,6 +34,56 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        //Moved + adapted from MainActivity
+
+        // I'm working on this part - Duy
+        Button profile_button = root.findViewById(R.id.profile_button);
+        profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), OrganizerEventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button MyEventButton = root.findViewById(R.id.my_event_button);
+        MyEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                builder.setTitle("Choose an action");
+
+                // Button to go to AttendeeEventActivity
+                builder.setPositiveButton("Go to Your Event Page (Attendee)", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(getContext(), AttendeeEventActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+                // Button to go to OrganizerEventActivity
+                builder.setNegativeButton("Go to Your Event Page (Organizer)", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(getContext(), OrganizerEventActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+                // Cancel button
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
         return root;
     }
 
