084ccefec14acc0b2fbedf03edb009cd1fa16b9e
assekho2 <157326482+assekho2@users.noreply.github.com>
2024-04-07 18:02:30 -0600

    Update build.gradle.kts

--- ReaderApp/QRCodeReader/app/build.gradle.kts
+++ ReaderApp/QRCodeReader/app/build.gradle.kts
@@ -90,4 +90,8 @@ dependencies {
     testImplementation("androidx.arch.core:core-testing:2.1.0")
     testImplementation ("org.mockito:mockito-core:3.3.3")
     testImplementation("org.robolectric:robolectric:4.3.1")
+
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("androidx.test:rules:1.4.0")
 }
